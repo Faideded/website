a9aa62933441246bc138ba3e3bcf7be68ca37b2d
Faideded <xw26vmvxjn@privaterelay.appleid.com>
2025-11-21 14:47:13 +0000

    Added a Background blur CSS object

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function showaboutme() {
      document.getElementById('aboutmepopup').style.visibility= 'visible';
      document.getElementById("abtmeexit").style.visibility= "visible";
+     document.querySelector(".Aboutmebackgroundblur").style.display = "block";
     var button = document. getElementById("button1");
-        button.disabled = true;
+    button.disabled = true;
     var button = document. getElementById("button2");
-        button.disabled = true;
+    button.disabled = true;
     }
 function homepagereload() {
     location = location
@@ -13,9 +14,10 @@ function homepagereload() {
 function hideaboutme() {
     document.getElementById('aboutmepopup').style.visibility= 'hidden';
     document.getElementById("abtmeexit").style.visibility= "hidden";
-        var button = document. getElementById("button1");
-        button.disabled = false;
-        var button = document. getElementById("button2");
-        button.disabled = false;
+    document.querySelector(".Aboutmebackgroundblur").style.display = "none";
+    var button = document. getElementById("button1");
+    button.disabled = false;
+    var button = document. getElementById("button2");
+    button.disabled = false;
 }
 
