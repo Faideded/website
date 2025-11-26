1975440a55aa6a1ffd987efb0a3837f4a2f4b3f3
Faideded <xw26vmvxjn@privaterelay.appleid.com>
2025-11-26 13:56:25 +0000

    animations working however not where i want them to be

--- index.js
+++ index.js
@@ -47,3 +47,18 @@ function hideDownload(){
     button = document.getElementById("button2");
     button.disabled = false;
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlidesfade");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); 
+}
